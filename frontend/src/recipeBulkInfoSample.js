9b0ea8396c364bfebbd9d6b99af4e34c1851c73f
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2023-03-16 22:16:42 -0500

    export sample responses

--- frontend/src/recipeBulkInfoSample.js
+++ frontend/src/recipeBulkInfoSample.js
@@ -1,4 +1,4 @@
-module.exports = [
+export default [
     {
         vegetarian: true,
         vegan: false,
--- frontend/src/recipeSearchSample.js
+++ frontend/src/recipeSearchSample.js
@@ -1,4 +1,4 @@
-module.exports = [
+export default [
     {
         id: 640352,
         title: "Cranberry Apple Crisp",
