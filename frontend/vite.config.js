4c50cdf540a6ea99ad9263dcdb4493610b3f56e3
bethany ann <bethanyann44@gmail.com>
2023-03-27 21:02:43 -0500

    Create vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    webfontDownload()
-  ],
+  plugins: [react()],
 })
