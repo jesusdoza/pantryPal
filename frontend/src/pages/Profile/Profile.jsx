e13f2c0051f83197546d355230f1c2190982cf96
jesus doza <jesus.doza@gmail.com>
2023-03-30 20:15:34 -0500

    Create Profile page

---
+++ frontend/src/pages/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Dashboard } from "./Profile.styles.jsx";
+import Cookies from "js-cookie";
+
+function ProfilePage() {
+    return <Dashboard>Dashboard here</Dashboard>;
+}
+
+export default ProfilePage;
