bfcdd8f359cbf616d7164710ad5a2582dcfc14fe
jesus doza <jesus.doza@gmail.com>
2023-04-01 16:59:16 -0500

    color pallet and color variables in profile styles

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -8,30 +8,43 @@ function ProfilePage() {
             <section className="profile-page">
                 <section className="container profile">
                     <picture>
-                        <img src="/src/assets/Spoon.png" alt="user avatar" />
+                        {/* <img src="/src/assets/Spoon.png" alt="user avatar" /> */}
+                        <source srcset="https://cdn.discordapp.com/attachments/1076184648599744674/1091575853470732288/PantryPal_Logo_TPB.png"></source>
+                        <img
+                            // src="https://cdn.discordapp.com/attachments/1076184648599744674/1091575853470732288/PantryPal_Logo_TPB.png"
+                            alt="user avatar"
+                        />
                     </picture>
                     <h3>Hi! Bob</h3>
                     <h3>Email@email.com</h3>
-                    <h4>Your Dietary Preferences</h4>
-                    <ul className="app-pref">
-                        <li value="gluten free">Gluten Free</li>
-                        <li value="ketogenic">Ketogenic</li>
-                        <li value="vegetarian">Vegetarian</li>
-                        <li value="lacto vegetarian">Lacto-Vegetarian</li>
-                        <li value="ovo-vegetarian">Ovo-Vegetarian</li>
-                        <li value="vegan">Vegan</li>
-                        <li value="pescetarian">Pescetarian</li>
-                        <li value="paleo">Paleo</li>
-                        <li value="primal">Primal</li>
-                    </ul>
                 </section>
                 <section className="container app-config">
+                    <section>
+                        <h4>Your Dietary Preferences</h4>
+                        <ul className="app-pref">
+                            <li>
+                                <div>
+                                    <span>Gluten Free</span>
+                                </div>
+                            </li>
+                            <li>Ketogenic</li>
+                            <li>Vegetarian</li>
+                            <li>Lacto-Vegetarian</li>
+                            <li>Ovo-Vegetarian</li>
+                            <li>Vegan</li>
+                            <li>Pescetarian</li>
+                            <li>Paleo</li>
+                            <li>Primal</li>
+                        </ul>
+                        <div className="controls">
+                            <span className="btn">
+                                Update Dietary Preferences
+                            </span>
+                        </div>
+                    </section>
                     <ul className="options">
                         <li className="btn">
                             <span>Update Caloric settings</span>
-                        </li>
-                        <li className="btn">
-                            <span>Update Dietary Preferences</span>
                         </li>
                         <li className="btn">
                             <span>Update Email</span>
