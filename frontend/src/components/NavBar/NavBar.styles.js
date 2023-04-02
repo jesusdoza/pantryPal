6456664b2fa5f8665c7058f6f594cfd5566844c7
Amanda Perry <challenge641@gmail.com>
2023-04-02 16:59:33 -0500

    feat: added media queries to navbar and home page for responsiveness (DSD100-64)

--- frontend/src/components/NavBar/NavBar.styles.js
+++ frontend/src/components/NavBar/NavBar.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { device } from '../mediaQ/device';
 
 export const Nav = styled.nav`
 
@@ -7,7 +8,8 @@ font-weight: 500;
 font-size: 1.5rem;
 display: flex;
 justify-content: space-around;
-align-items: center;
+align-items: top;
+margin-top: 10px;
 
 img {
   height: auto;
@@ -21,10 +23,67 @@ img {
 
 
 a {
-  display: flex;
-  justify-content: space-around;
   color: #90ac14;
   text-decoration: none;
+}
+
+
+@media ${device.mobileS} { 
+  min-width: 200px;
+  font-size: .9rem;
+  .logo {
+    height: 50px;
+    width: 54px;
+  }
+}
+
+@media ${device.mobileM} { 
+  min-width: 320px;
+  font-size: .9rem;
+  .logo {
+    height: 50px;
+    width: 54px;
+  }
+}
+
+@media ${device.mobileL} { 
+  min-width: 375px;
+  font-size: 1rem;
+  .logo {
+    height: 50px;
+    width: 54px;
+  }
+}
+
+@media ${device.tablet} { 
+  min-width: 425px;
+}
+
+@media ${device.laptop} { 
+  min-width: 768px;
+  font-size: 1.8rem;
+  .logo {
+    height: 100px;
+    width: 120px;
+  }
+}
+
+@media ${device.desktop} {
+  min-width: 1441px;
+  font-size: 3rem;
+  .logo {
+    height: 140px;
+    width: 160px;
+  }
+}
+
+@media ${device.desktopL} {
+  min-width: 2560px;
+  font-size: 4rem;
+  .logo {
+    height: 280px;
+    width: 320px;
+  }
 }
 
 `;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,9 +4,15 @@ import PantryPalText from '../../assets/PantryPalText.png'
 import { ImgContainer } from './Home.styles';
 
 
+
 const Home = () => {
+
+
+
   return (
+
     <ImgContainer>
+
       <h1>Find recipes | Meal Plan</h1>
       <img className="logo" src={JustEgg} alt="Egg"></img>
       <img className="logoText" src={PantryPalText} alt="PantryPal"></img>
