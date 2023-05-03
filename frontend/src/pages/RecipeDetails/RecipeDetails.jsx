ed786d0910630fab4b3f3279cccb76385bd588b4
Damian Padilla <62222815+Damianpad@users.noreply.github.com>
2023-05-02 20:14:19 -0500

    update: updated design of Recipe Details and nav (#19) (#22)
    * update: updated design of Recipe Details and nav (#19)
    
    * fix: added uuid to remove unique key errors(#19)

--- frontend/src/pages/RecipeDetails/RecipeDetails.jsx
+++ frontend/src/pages/RecipeDetails/RecipeDetails.jsx
@@ -1,6 +1,10 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { useLocation,useNavigate } from "react-router-dom";
 import { Details } from './RecipeDetails.style';
+import { v4 as uuidv4 } from "uuid";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faEgg, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function RecipeDetails() {
@@ -18,42 +22,88 @@ export default function RecipeDetails() {
 
 
   const recipeType = recipeData.dishTypes.map(type => {
-    return <li>{type}</li>
+    return <li key={uuidv4()}>{type}</li>
   })
   const ingredientName = recipeData.extendedIngredients.map(ingredient => {
-    return <li>{ingredient.amount} {ingredient.unit}  <img src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`}></img> {ingredient.name}</li>})
+    return <li key={uuidv4()}>{ingredient.amount} {ingredient.unit}  <img src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`}></img> {ingredient.name}</li>})
+  
+  console.log(recipeData);
 
 
   const instructionStep = recipeData.analyzedInstructions[0].steps.map(instruction => {
-    return <li>{instruction.step}</li>})
+    return <li key={uuidv4()}>{instruction.step}</li>})
 
-
+    const strippedString = recipeData.summary.replace(/<.*?>/g, '');
+      const ingredientLength = recipeData.extendedIngredients.length
+  const likes = recipeData.aggregateLikes
+  const recipeDiets = recipeData.diets.map((item) => {
+    return <li key={uuidv4()}>{item}</li>
+  })
 
   return (
     <Details>
-      <div className='recipeHeader'>
-        <div className='title'>
-          <h1 >{recipeData.title}</h1>
-          <h2 className='recipeTime'>Ready in {recipeData.readyInMinutes} minutes</h2>
-        </div>
-
-
-        <div className='recipeHead'>
-          <div className='recipeCategory'>
-          {recipeType}
-        </div>
-
-        <div className='recipeStart'>
-          <div>
-            <img 
+      <div className='recipeContainer'>
+        <div className='recipeImgContainer'>
+          <div className='imageContainer'>
+          <img 
             className='recipeImg'
             src={recipeData.image} 
             alt={recipeData.title} 
             />
+            
+            <button className='saveBtn'><FontAwesomeIcon icon={faHeart} className='saveIcon'/></button>
           </div>
 
+
+            <section className='recipeDetails'>
+              <div className='titleContainer'>
+                <h1 className='recipeTitle'>{recipeData.title}</h1>
+                
+              </div>
+
+              <div className='quickInfoContainer'>
+                <section className='quickInfoItem'>
+                <FontAwesomeIcon icon={faClock} className='quickIcon'/>
+                
+                  <h2 className='recipeTime'>{recipeData.readyInMinutes} minutes</h2>
+                </section>
+              <section>
+              <FontAwesomeIcon icon={faEgg} className='quickIcon'/>
+              <h2 className='recipeTime'>{ingredientLength} Ingredients</h2>
+              </section>
+              
+              <section>
+              <FontAwesomeIcon icon={faHeart} className='quickIcon'/>
+              <h2 className='recipeTime'>{likes} Likes</h2>
+              </section>
+              </div>
+
+              <section className='recipeSummary'>
+                <p>{strippedString}</p>
+              </section>
+              
+              <section className='recipeTags'>
+                <div className='recipeCategory'>
+                  {recipeType}
+                </div>
+                <div className='recipeDiets'>
+                  {recipeDiets}
+                </div>
+              </section>
+            </section>
+            
+        </div>
+
+
+        <div className='recipeHead'>
+
+
+        <div className='recipeStart'>
+
           <div className='ingredientCard'>
-            <h2>What you'll need:</h2>
+              <h2>Servings: {recipeData.servings}</h2>
+              <input type="number" placeholder={recipeData.servings}/>
+              <h2>What you'll need:</h2>
             <div className='recipeIngredient'>
               {ingredientName}
             </div>
@@ -62,10 +112,27 @@ export default function RecipeDetails() {
       </div>
 
         <div className='instructionsCard'>
-          <h2>Instructions:</h2>
-          <ol>
-          {instructionStep}
-          </ol>
+        <h2>{recipeData.title}</h2>
+
+        <div className='instructionContainer'>
+        <div>
+        <img 
+            className='instructionImg'
+            src={recipeData.image} 
+            alt={recipeData.title} 
+          />
+        </div>
+
+
+          <div className='recipeInstructions'>
+            <h2>Instructions:</h2>
+            <ol>
+            {instructionStep}
+            </ol>
+          </div>
+        </div>
+
+
         </div>
       </div>
     </Details>
