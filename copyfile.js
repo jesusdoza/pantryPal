9df16a6b1fdce47f56fc895838a3cdb2a778adbd
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2023-06-16 22:02:57 -0500

    removed dot env

--- copyfile.js
+++ copyfile.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const envFile = path.normalize("./env.production");
+const envFile = path.normalize("/etc/secrets/env.production");
 const newEnvFile = path.normalize("./frontend/env.production");
 
 if (fs.existsSync(newEnvFile)) {
