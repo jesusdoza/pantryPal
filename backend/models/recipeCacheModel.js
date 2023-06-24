b756ca04bd0ab81dcd2309254b2313ec3de7e159
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2023-06-24 18:14:39 -0500

    update cache model with createdAt field

--- backend/models/recipeCacheModel.js
+++ backend/models/recipeCacheModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const recipeCacheSchema = new mongoose.Schema({
     key: { type: String, required: true, unique: true },
     data: [Object],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 const RecipeCache = mongoose.model("RecipeCache", recipeCacheSchema);
